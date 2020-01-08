3388e1af7c843ac9980589c46a6606a20409cef3
Shaun Garwood <shaun.garwood@charter.com>
2020-01-08 09:20:39 -0700

    smarter initial bootsrap

--- bin/initial-bootstrap.sh
+++ bin/initial-bootstrap.sh
@@ -4,11 +4,56 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-apt-get install software-properties-common
-apt-add-repository ppa:ansible/ansible
-apt-get update
-apt-get install ansible
+# stole this from: https://unix.stackexchange.com/a/6348
+if [ -f /etc/os-release ]; then
+    # freedesktop.org and systemd
+    . /etc/os-release
+    OS=$NAME
+    VER=$VERSION_ID
+elif type lsb_release >/dev/null 2>&1; then
+    # linuxbase.org
+    OS=$(lsb_release -si)
+    VER=$(lsb_release -sr)
+elif [ -f /etc/lsb-release ]; then
+    # For some versions of Debian/Ubuntu without lsb_release command
+    . /etc/lsb-release
+    OS=$DISTRIB_ID
+    VER=$DISTRIB_RELEASE
+elif [ -f /etc/debian_version ]; then
+    # Older Debian/Ubuntu/etc.
+    OS=Debian
+    VER=$(cat /etc/debian_version)
+elif [ -f /etc/SuSe-release ]; then
+    # Older SuSE/etc.
+    ...
+elif [ -f /etc/redhat-release ]; then
+    # Older Red Hat, CentOS, etc.
+    ...
+else
+    # Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.
+    OS=$(uname -s)
+    VER=$(uname -r)
+fi
 
-# find a way to determine if it's centos
-#yum install epel-release
-#yum install ansible
+echo $OS
+echo $VER
+
+# install ansible and git
+if [ $OS == "Ubuntu" ] || [ $OS == "Debian" ]; then
+    echo "this is debian flavor machine"
+
+    apt-get install software-properties-common
+    apt-add-repository ppa:ansible/ansible
+    apt-get update
+    apt-get install git ansible
+elif [ $OS == CentOS* ]; then
+    echo "this is centos machine"
+    yum install epel-release
+    yum install ansible
+else
+    echo "not sure what flavor this machine is"
+    echo "exiting"
+    exit
+fi
+
+git clone git@github.com:shaungarwood/my_bootstraps.git
